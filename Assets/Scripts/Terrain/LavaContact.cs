82829c924d39a91fb0bbd42f2d274ea024b8466a
Teddy <102239127+Onsraa@users.noreply.github.com>
2023-04-27 01:21:15 +0200

    mobs and menu scripts

---
+++ Assets/Scripts/Terrain/LavaContact.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Terrain
+{
+    public class LavaContact : MonoBehaviour
+    {
+        public string sceneName;
+    
+        private void OnTriggerEnter(Collider other)
+        {
+            GameOver(other.gameObject);
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            GameOver(other.gameObject);
+        }
+
+        private void GameOver(GameObject other)
+        {
+            if (!other.CompareTag("Player"))
+            {
+                return;
+            }
+
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
